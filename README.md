e04f92b788701fdadb3520d37dc5db58f3903127
Helder <h.alcantara8@hotmail.com>
2021-04-04 19:14:18 -0300

    Add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+-Instalar o kNEX de forma global
+    npm install knex -g
+
+-Configurar o arquivo .env
+    Mudar nome da base de dados
+    Credencias do banco de dados
+    Criar encode key para JWT
+
+- Rodar o comando parar usar as migrations e criar as tabelas
+    - knex migrate:latest
+    - knex seed:run
+
+- Comandos uteis Knex - (http://knexjs.org/)
+    Criar migration: knex migrate:make migration_name 
+    Criar seeds: knex seed:make seed_name
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Multer = require('multer');
 
 
 const Middlewares = require('./middlewares/middlewares')
@@ -14,29 +13,9 @@ router.get('/', HomeController.index);
 router.post('/login', LoginController.login);
 
 
-// router.use(Middlewares.auth);
+router.use(Middlewares.auth);
 router.get('/users', UserController.index);
 router.post('/users/store', UserController.store);
 router.get('/logout', LoginController.logout);
-
-
-
-router.get('/image', async (req, res)=>{
-    res.json({any: 'ddd'});
-});
-
-
-//---------------- uploads ------------------------
-const upload = require('./helpers/upload');
-
-const multer = Multer({
-    storage: Multer.memoryStorage(),
-    limits: {
-      fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
-    }
-});
-
-router.post('/upload', multer.single('file'), upload.uploadFile);
-
 
 module.exports = router;
